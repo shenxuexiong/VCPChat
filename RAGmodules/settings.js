7bcf009cc3d213ac3f4df8e86cbd1c025839998d
lioensky <140802180+lioensky@users.noreply.github.com>
2025-10-17 16:50:58 +0800

    Delete RAGmodules/settings.js

--- RAGmodules/settings.js
+++
@@ -1,7 +0,0 @@
-// VCP Settings - Auto-generated from AppData/settings.json
-// 这个文件会被主程序自动更新
-window.VCP_SETTINGS = {
-    "vcpLogUrl": "ws://192.168.2.179:6005",
-    "vcpLogKey": "123456",
-    "currentThemeMode": "dark"
-};
